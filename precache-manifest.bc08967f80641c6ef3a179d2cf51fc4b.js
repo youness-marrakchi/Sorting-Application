faff465bc229b6704d3728315e26554aba43684f
youness marrakchi <suhanesswork@gmail.com>
2019-10-21 10:11:31 +0000

    Updates

---
+++ precache-manifest.bc08967f80641c6ef3a179d2cf51fc4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7373fbebaf074be076a7eae595d8dae5",
+    "url": "/Sorting-Application/index.html"
+  },
+  {
+    "revision": "e5b7b6521d63bdf0ff64",
+    "url": "/Sorting-Application/static/css/main.455fed2b.chunk.css"
+  },
+  {
+    "revision": "154dccf1c058ee1bec48",
+    "url": "/Sorting-Application/static/js/2.081899d7.chunk.js"
+  },
+  {
+    "revision": "e5b7b6521d63bdf0ff64",
+    "url": "/Sorting-Application/static/js/main.9b7dd7a4.chunk.js"
+  },
+  {
+    "revision": "80a0228c326f6ca6458c",
+    "url": "/Sorting-Application/static/js/runtime-main.31678013.js"
+  }
+]);
